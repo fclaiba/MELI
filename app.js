d87086a8eb26ac36def467914e9b270ac4fe5703
fclaiba <f.claisse.r@gmail.com>
2023-05-03 12:38:26 -0300

    proyecto terminado

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'views/home.html'));
 });
 
+app.get('/login', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views/login.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views/register.html'));
+});
+
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
