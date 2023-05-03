ab83371954db8552afbd499bd59d96434b579603
fclaiba <f.claisse.r@gmail.com>
2023-05-03 18:29:49 -0300

    pry1

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ app.get('/register', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'views/register.html'));
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Servidor ejecutándose en el puerto ${port}`);
-});
+app.listen(process.env.PORT || 3000, function() { // 3000 o el puerto que usen
+  console.log("Servidor corriendo");
+})
