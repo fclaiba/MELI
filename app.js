a789910dd566149db0ccf14faec03f8c053f49be
fclaiba <f.claisse.r@gmail.com>
2023-06-30 15:27:12 -0300

    retoques al pry final

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-const path = require('path');
 const express = require('express');
-
+const path = require("path");
 const app = express();
 
-const publicPath = path.resolve(__dirname, 'public');
-app.use(express.static(publicPath));
+app.use('/static', express.static(__dirname + '/public'))
+app.use(express.static('public'))
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+})
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'views/home.html'));
-});
-
-app.get('/login', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'views/login.html'));
-});
+    res.sendFile(__dirname + '/views/home.html')
+})
 
 app.get('/register', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'views/register.html'));
-});
+    res.sendFile(path.resolve('./views/register.html'))
+})
 
-app.listen(process.env.PORT || 3000, function() { // 3000 o el puerto que usen
-  console.log("Servidor corriendo");
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve('./views/login.html'))
 })
